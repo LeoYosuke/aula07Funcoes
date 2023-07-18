a6bc9def1ac1f64d6810a7ff3b7c248662821151
Leonardo Yosuke Konichi <135863260+LeoYosuke@users.noreply.github.com>
2023-07-18 17:26:23 -0300

    Add files via upload

---
+++ aula07Funcoes/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace aula07Funcoes
+{
+    partial class BTNmostrar
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.LBLdindin = new System.Windows.Forms.Label();
+            this.BTNsalvar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(47, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mostre a mensagem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(47, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Mostra outra coisa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(47, 156);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(180, 20);
+            this.txt1.TabIndex = 2;
+            // 
+            // LBLdindin
+            // 
+            this.LBLdindin.AutoSize = true;
+            this.LBLdindin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLdindin.Location = new System.Drawing.Point(538, 155);
+            this.LBLdindin.Name = "LBLdindin";
+            this.LBLdindin.Size = new System.Drawing.Size(48, 18);
+            this.LBLdindin.TabIndex = 3;
+            this.LBLdindin.Text = "R$ 0 ";
+            // 
+            // BTNsalvar
+            // 
+            this.BTNsalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNsalvar.Location = new System.Drawing.Point(541, 266);
+            this.BTNsalvar.Name = "BTNsalvar";
+            this.BTNsalvar.Size = new System.Drawing.Size(75, 23);
+            this.BTNsalvar.TabIndex = 4;
+            this.BTNsalvar.Text = "salvar";
+            this.BTNsalvar.UseVisualStyleBackColor = true;
+            this.BTNsalvar.Click += new System.EventHandler(this.BTNsalvar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(541, 210);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(503, 140);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(174, 179);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(13, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 186);
+            this.panel2.TabIndex = 7;
+            // 
+            // BTNmostrar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BTNsalvar);
+            this.Controls.Add(this.LBLdindin);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "BTNmostrar";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label LBLdindin;
+        private System.Windows.Forms.Button BTNsalvar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
